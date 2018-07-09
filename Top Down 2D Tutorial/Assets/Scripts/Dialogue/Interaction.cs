afd64f77f7a89d94e30d764947e241ffdd13cb5a
Ron-Ash <a158.ron@gmail.com>
2018-07-09 14:31:21 +1000

    dialogue starts when clicking on NPC

---
+++ Top Down 2D Tutorial/Assets/Scripts/Dialogue/Interaction.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Interaction : MonoBehaviour {
+
+	private ModalPanel modalPanel;
+
+	private UnityAction yesEvent;
+	private UnityAction noEvent;
+	private UnityAction cancelEvent;
+
+	void Awake()
+	{
+		modalPanel = ModalPanel.Instance();
+
+		yesEvent = new UnityAction(Testbutton1);
+		noEvent = new UnityAction(Testbutton2);
+		cancelEvent = new UnityAction(TestCancel);
+	}
+
+	void OnMouseDown()
+    {
+        Debug.Log("Hello");
+		if(gameObject.tag == "NPC")
+		{
+			Debug.Log("I dont like you!!!");
+			modalPanel.Choice("Would you like a poke in the eye?\nHow about with a sharp stick?", yesEvent, noEvent, cancelEvent);
+		}
+		else
+		{
+			Debug.Log(gameObject.name);
+		}
+    }
+
+	void Testbutton1()
+	{
+		Debug.Log("Heck yeah!!!");
+	}
+
+	void Testbutton2()
+	{
+		Debug.Log("NOOOO!!!");
+	}
+
+	void TestCancel()
+	{
+		Debug.Log("I give up");
+	}
+}
